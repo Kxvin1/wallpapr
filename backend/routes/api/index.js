99455aa3ba1e69193915aed1d7c20a10ee436061
kevin <kevinzy17@gmail.com>
2022-04-26 20:28:35 -0700

    tested middleware (they are working properly)

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,43 @@
 const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
+// User model
+const { User } = require("../../db/models");
+
+// utils
+const { setTokenCookie } = require("../../utils/auth.js");
+const { restoreUser } = require("../../utils/auth.js");
+const { requireAuth } = require("../../utils/auth.js");
 
 module.exports = router;
+
+// Test Routes Below (uncomment if want to test!)
+// // test route (simple one)
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
+// // test route (for set token cookie) -- this sets the token (in dev tools > application > "token")
+// router.get(
+//   "/set-token-cookie",
+//   asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//       where: {
+//         username: "Demo-lition",
+//       },
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+//   })
+// );
+
+// // test route (for restore user) -- this restores the user if a token was set
+// router.get("/restore-user", restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// // test route (for require auth) -- if there is no session user, it will return an error
+// // otherwise, it will return the session user's information
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
