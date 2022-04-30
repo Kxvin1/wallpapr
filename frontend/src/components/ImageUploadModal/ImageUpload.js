4fd8316d2f1dc23417673634fae1572b825b8d0e
kevin <kevinzy17@gmail.com>
2022-04-30 16:28:39 -0700

    commented out usehistory

--- frontend/src/components/ImageUploadModal/ImageUpload.js
+++ frontend/src/components/ImageUploadModal/ImageUpload.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { postImage } from "../../store/images";
 
 import "./ImageUpload.css";
