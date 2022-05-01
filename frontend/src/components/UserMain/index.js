3d5889a67a1a47e524f5a611969c66e46907a74b
kevin <kevinzy17@gmail.com>
2022-04-30 23:00:01 -0700

    refactored to prepare for images CRUD on main page

--- frontend/src/components/UserMain/index.js
+++ frontend/src/components/UserMain/index.js
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { getImages } from "../../store/images";
-// import { Modal } from "../../context/Modal";
+import ImageInfo from "../ImageInfo";
 
 import "./UserMain.css";
 
 function UserMain() {
   const dispatch = useDispatch();
   // const [showModal, setShowModal] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user);
   const [model, setModel] = useState(false);
   const [tempimgSrc, setTempImgSrc] = useState("");
   const [modelId, setModelId] = useState("");
-
-  // const sessionUser = useSelector((state) => state.session.user);
 
   const imagesObject = useSelector((state) => state.image);
   const images = Object.values(imagesObject);
@@ -35,28 +34,14 @@ function UserMain() {
   };
 
   return (
-    <div>
-      <div
-        className={model ? "model open" : "model"}
-        onClick={() => setModel(false)}
-      >
-        <img className="modal_img" src={tempimgSrc} alt="img"></img>
-      </div>
-      <div className="image-row">
-        {images.map((image) => {
-          return (
-            <div className="image-block" key={image.id}>
-              <img
-                onClick={() => getImg(image.imageURL, image.id)}
-                className="image-poster"
-                src={image.imageURL}
-                alt="img-alt"
-                key={image.id}
-              ></img>
-            </div>
-          );
-        })}
-      </div>
+    <div className="image-user-main-container">
+      {images?.map((image) => {
+        if (image.userId === sessionUser.id) {
+          return <ImageInfo key={image.id} image={image} />;
+        }
+
+        return <ImageInfo key={image.id} image={image} />;
+      })}
     </div>
   );
 }
