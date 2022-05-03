fe4f02b8a17026b9ec1e3b9a8f2ddf7feaf44e8a
kevin <kevinzy17@gmail.com>
2022-05-02 23:31:13 -0700

    somewhat working info component

--- frontend/src/components/CommentInfo/index.js
+++ frontend/src/components/CommentInfo/index.js
@@ -6,13 +6,16 @@ import "./CommentInfo.css";
 
 function CommentInfo({ comment }) {
   const sessionUser = useSelector((state) => state.session.user);
+  // console.log(comment);
 
   const date = new Date(comment.createdAt).toDateString();
+
+  // console.log(comment.User?.username);
 
   return (
     <div className="comment-container">
       <div className="commented-by-container">
-        <div className="commented-by">Comment by: {comment.User.username}</div>
+        <div className="commented-by">Comment by: {comment.User?.username}</div>
       </div>
       <div className="comment-text">{comment.commentText}</div>
       <div className="comment-date">{date}</div>
