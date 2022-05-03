7ccbc419fcd99c59bf8171542f7a966721c26c6a
kevin <kevinzy17@gmail.com>
2022-05-02 19:32:42 -0700

    created comment info component

---
+++ frontend/src/components/CommentInfo/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+// import delete comment modal
+import "./CommentInfo.css";
+
+function CommentInfo({ comment }) {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  const date = new Date(comment.createdAt).toDateString();
+
+  return (
+    <div className="comment-container">
+      <div className="commented-by-container">
+        <div className="commented-by">Comment by: {comment.User.username}</div>
+      </div>
+      <div className="comment-text">{comment.commentText}</div>
+      <div className="comment-date">{date}</div>
+      {/* add when delete comment modal is created and if match with userId then allow delete
+      {sessionUser.id === comment.userId && <DeleteCommentModal comment={comment} />} */}
+    </div>
+  );
+}
+
+export default CommentInfo;
