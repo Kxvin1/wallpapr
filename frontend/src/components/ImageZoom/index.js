15aeed975e778b1dbdbb50f1615828e0176d0fde
kevin <kevinzy17@gmail.com>
2022-05-03 22:28:16 -0700

    fixed image zoom with user profile link

--- frontend/src/components/ImageZoom/index.js
+++ frontend/src/components/ImageZoom/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ImageZoom.css";
 import { NavLink } from "react-router-dom";
+import { getUsers } from "../../store/user";
+import { useDispatch, useSelector } from "react-redux";
 
 function ImageZoom({ image, tagString }) {
-  // console.log(image);
-  // console.log("tag string", tagString);
+  const dispatch = useDispatch();
+  const usersObj = useSelector((state) => state.user);
+  const users = Object.values(usersObj);
+
+  // console.log("users", users);
+
+  const username = users.find((user) => {
+    return user.id === image.userId;
+  });
+
+  console.log("~~~~~~~~~~~~username~~~~~~~~~~~~~", username?.username);
 
   // {id: 2, userId: 4, imageURL: '/images/image-2.png', tags: Array(3), favoritedCount: 151, …}
   // Favorites: []
@@ -17,17 +28,23 @@ function ImageZoom({ image, tagString }) {
   // updatedAt: "2022-04-29T00:45:22.345Z"
   // userId: 4
 
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
   return (
     <div className="image-zoom-container" key={image.id}>
       <img className="image-zoom-zoomed" src={image.imageURL} alt="img" />
       <div className="image-zoomed-info">
-        {/* <NavLink
-          className="zoomed-uploader-container"
-          to={`/profiles/${image?.userId}`}
-        >
-          <p className="zoomed-uploader-name">{image?.User.username}</p>
-        </NavLink> */}
         <div className="image-zoom-all-tags" key={image}>
+          <NavLink
+            className="zoomed-uploader-container"
+            to={`/profiles/${image?.userId}`}
+          >
+            <p className="zoomed-uploader-name">
+              {username?.username}'s Profile
+            </p>
+          </NavLink>
           {tagString ? (
             tagString
               .split(",")
