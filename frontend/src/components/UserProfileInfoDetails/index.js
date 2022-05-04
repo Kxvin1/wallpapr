7f6facc46054fcfbd25c586407ad7421e7812310
kevin <kevinzy17@gmail.com>
2022-05-04 10:45:10 -0700

    added uploads link to profile page

--- frontend/src/components/UserProfileInfoDetails/index.js
+++ frontend/src/components/UserProfileInfoDetails/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { getUserProfile } from "../../store/profile";
 // import { loadMemberProfileThunk } from "../../store/member"; // doesn't work
+import { getUsers } from "../../store/user";
 
 import "./UserProfileInfoDetails.css";
 
@@ -11,7 +13,43 @@ function UserProfileInfoDetails({ memberId }) {
   const profileInfo = Object.values(sessionProfile);
   const actualProfile = profileInfo[0];
 
-  console.log("actual", actualProfile);
+  // console.log("actual", actualProfile);
+
+  const usersObj = useSelector((state) => state.user);
+  const users = Object.values(usersObj);
+
+  // const uploaderProfile = users.find((user) => {
+  //   return user.id === image.userId;
+  // });
+
+  const userUploadsLinkObj = profileInfo[0];
+
+  // console.log(userUploadsLinkObj?.User?.id);
+  // console.log(userUploadsLinkObj?.User?.username);
+
+  const userUploaderId = userUploadsLinkObj?.User?.id;
+  const userUploaderUsername = userUploadsLinkObj?.User?.username;
+
+  console.log(userUploaderId);
+  console.log(userUploaderUsername);
+
+  let collectionLinks;
+
+  if (userUploaderId) {
+    collectionLinks = (
+      <p className="user-collection-link">
+        <NavLink
+          className="user-collection-navlink"
+          to={`/members/${userUploaderId}`}
+        >
+          {userUploaderUsername}'s Collection
+        </NavLink>
+      </p>
+    );
+  } else {
+    collectionLinks = <></>;
+  }
+
   // console.log("actual profile", actualProfile);
   // console.log("sesh user", actualProfile.userId === 7);
   // array of objs containing user profile stuff.
@@ -36,9 +74,10 @@ function UserProfileInfoDetails({ memberId }) {
           src="/logo-images/1-default-avatar-astro.png"
           alt="user"
         />
+        {collectionLinks}
         <h3>{actualProfile?.fullName}</h3>
-        <h3>AKA: {actualProfile?.User?.username}</h3>
-        <h6>From: {actualProfile?.location}</h6>
+        <h4>AKA: "{actualProfile?.User?.username}"</h4>
+        <h3>Location: {actualProfile?.location}</h3>
         <p className="user-bio">{actualProfile?.biography}</p>
         {/* EditProfileModal below: */}
         {/* {sessionUser.id === +memberId && <EditProfileModal myProfile={actualProfile} />} */}
